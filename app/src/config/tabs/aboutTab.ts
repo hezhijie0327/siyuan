d53b63d692af987eea3e43b3b8ef839a618b3b2c
Zhijie He <hezhijie0327@hotmail.com>
2026-06-22 21:20:27 +0800

    fix: remove patch

--- app/src/config/tabs/aboutTab.ts
+++ app/src/config/tabs/aboutTab.ts
@@ -40,6 +40,7 @@ const genAboutVersionHtml = (): string => {
     return `<div class="fn__flex b3-label config-item config-wrap">
     <div class="fn__flex-1">
         <div class="config-name">${window.siyuan.languages.currentVer} v${Constants.SIYUAN_VERSION}<span id="isInsider"></span></div>
+        <div class="b3-label__text">${window.siyuan.languages.downloadLatestVer}</div>
     </div>
     <div class="fn__space"></div>
     <div class="fn__flex-center fn__size200">
